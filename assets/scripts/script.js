8bab885a25912f42697f307f55cd15ae43e1e1bf
Miles Bosch <milbosrej@alu.edu.gva.es>
2024-02-24 18:13:35 +0100

    feat: add a sticky nav and match footer

---
+++ assets/scripts/script.js
@@ -0,0 +1,9 @@
+// TMP:
+document.getElementById('menu').addEventListener('click', function () {
+  const nav = document.querySelector('nav');
+  let nav2 = nav.cloneNode(true);
+  nav2.className = 'nav2';
+  document.getElementById('menu').append(nav2);
+  nav2 = document.querySelector('#menu .nav2');
+  nav2.style.display === 'flex' ? nav2.style.display = 'none' : nav2.style.display = 'flex';
+});
