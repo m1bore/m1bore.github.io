47ea8df1acb036645eb14586b4241704253eb045
Miles Bosch <milbosrej@alu.edu.gva.es>
2024-02-24 19:46:34 +0100

    fix: add if in menu function

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -3,8 +3,11 @@ document.getElementById('menu').addEventListener('click', function () {
   const nav = document.querySelector('nav');
   let nav2 = nav.cloneNode(true);
   nav2.className = 'nav2';
-  document.getElementById('menu').append(nav2);
+  if (!document.querySelector('.nav2')) {
+    document.getElementById('menu').append(nav2);
+  }
   nav2 = document.querySelector('#menu .nav2');
-  nav2.style.display === 'flex' ? nav2.style.display = 'none' : 
-    nav2.style.display = 'flex';
+  nav2.style.display === 'flex'
+    ? nav2.style.display = 'none'
+    : nav2.style.display = 'flex';
 });
